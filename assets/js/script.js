2542bb897740d74f8d929909d5ebf8ae981c7ab2
Luiz Eduardo <luiz.edsfn@grupocev.com>
2023-06-29 20:43:15 -0300

    .

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function hideScreenLock(){ 
+    let screenlock = document.getElementById("screenlock");
+    screenlock.className = "hide";
+}
+
+function showScreenLock(){
+    let screenlock = document.getElementById("screenlock");
+    screenlock.className = "show";
+}
